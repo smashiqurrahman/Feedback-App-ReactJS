b7e8c92401c0f2d687e616dc7b702858dc6794f1
Ashiq <ashiqurrahmansm@gmail.com>
2022-03-02 17:22:26 +0600

    adding json-server as backend

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,33 +2,35 @@ import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem'
 import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from '../shared/Spinner'
 
 function FeedbackList() {
 
-    const { feedback } = useContext(FeedbackContext)
+    const { feedback, isLoading } = useContext(FeedbackContext)
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>;
     }
 
-    return (
-        <div className='feedback-list'>
-            <AnimatePresence>
-                {feedback.map((item) => (
-                    <motion.div
+    return isLoading ? (<Spinner />) :
+    (<div className='feedback-list'>
+        <AnimatePresence>
+            {feedback.map((item) => (
+                <motion.div
+                    key={item.id}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    <FeedbackItem
                         key={item.id}
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                        >
-                        <FeedbackItem
-                            key={item.id}
-                            item={item}                            
-                        />
-                    </motion.div>
-                ))}
-            </AnimatePresence>
-        </div>
+                        item={item}
+                    />
+                </motion.div>
+            ))}
+        </AnimatePresence>
+    </div>)
+
 
         // return (
         //     <div className='feedback-list'>
@@ -40,7 +42,7 @@ function FeedbackList() {
         //             />
         //         ))}
         //     </div>
-    )
+    
 }
 
 
